ef2a9d26ae9e0b87a11bed64a04263f6d9bb9af2
Dan Cohen <dan74c@gmail.com>
2017-09-26 23:32:39 +0300

    messing with phaser - client & server

---
+++ Client/client.js
@@ -0,0 +1,116 @@
+var game = new Phaser.Game(1200, 1000, Phaser.AUTO);
+
+
+var Client = {};
+Client.socket = io.connect();
+
+
+
+
+
+
+
+
+var GameState = {
+
+    preload: function () {
+        this.load.image('background', 'assets/backround.jpg');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.image('blast', 'assets/blast.png');
+
+
+
+
+        Client.socket.on('newPositions', function (data) {
+            console.log("I've recieved a message");
+        });
+
+        document.onkeydown = function (event) {
+            if (event.keyCode === 68) //d
+                socket.emit('keyPress', {
+                    inputId: 'right',
+                    state: true
+                });
+            else if (event.keyCode === 83) //s
+                socket.emit('keyPress', {
+                    inputId: 'down',
+                    state: true
+                });
+            else if (event.keyCode === 65) //a
+                socket.emit('keyPress', {
+                    inputId: 'left',
+                    state: true
+                });
+            else if (event.keyCode === 87) // w
+                socket.emit('keyPress', {
+                    inputId: 'up',
+                    state: true
+                });
+
+        }
+        document.onkeyup = function (event) {
+            if (event.keyCode === 68) //d
+                socket.emit('keyPress', {
+                    inputId: 'right',
+                    state: false
+                });
+            else if (event.keyCode === 83) //s
+                socket.emit('keyPress', {
+                    inputId: 'down',
+                    state: false
+                });
+            else if (event.keyCode === 65) //a
+                socket.emit('keyPress', {
+                    inputId: 'left',
+                    state: false
+                });
+            else if (event.keyCode === 87) // w
+                socket.emit('keyPress', {
+                    inputId: 'up',
+                    state: false
+                });
+        }
+
+
+
+    },
+    create: function () {
+
+        Client.socket.on('newPositions',function(data){
+            game.world.removeAll();
+            game.background = game.add.sprite(0, 0, 'background');
+            for (var i=0;i<data.bombs.length;i++){
+                var bomb = data.bombs[i];
+                game.add.sprite(bomb.x,bomb.y,'bomb');
+            }
+            
+        });
+
+
+        this.background = this.game.add.sprite(0, 0, 'background');
+        this.bomb = this.game.add.sprite(0, 0, 'bomb');
+        this.bomb.enlarge = true;
+    },
+    update: function () {
+
+        if (this.bomb.enlarge == true) {
+            this.bomb.scale.x += 0.01;
+            this.bomb.scale.y += 0.01;
+            if (this.bomb.scale.x > 1.2) {
+                this.bomb.enlarge = false;
+            }
+        } else {
+            this.bomb.scale.x -= 0.01;
+            this.bomb.scale.y -= 0.01;
+            if (this.bomb.scale.x < 0.9) {
+                this.bomb.enlarge = true;
+            }
+        }
+
+
+
+    }
+};
+
+game.state.add("GameState", GameState);
+game.state.start("GameState");
--- server.js
+++ server.js
@@ -4,95 +4,119 @@ var app = express();
 var serv = require('http').Server(app);
 
 //Game module
+/*
 var Game = require('Classes/Game');
 var game = new Game();
- 
+*/
 
-app.use('/',express.static(__dirname + '/client'));
+
+app.use('/', express.static(__dirname + '/client'));
+
 
 //Server startup
 serv.listen(2000);
 console.log("Server started.");
- 
+
+
 //Decllaring socket list
 var SOCKET_LIST = {};
-var io = require('socket.io')(serv,{});
+var io = require('socket.io')(serv, {});
 
 //On connection - when a client setup a connection with the server,
 //do the following:
-io.sockets.on('connection', function(socket){
+io.sockets.on('connection', function (socket) {
     //Create random id for the socket
     socket.id = Math.random();
     //add the new session into the socket list
     SOCKET_LIST[socket.id] = socket;
- 
+
     //create new player
-    var player = Game.createPlayer(socket.id); //a player object should be returned here
-   
+    /* var player = Game.createPlayer(socket.id); //a player object should be returned here */
+
     //when this session disconnect do the following:
+    /*
     socket.on('disconnect',function(){
         //delete socket
         delete SOCKET_LIST[socket.id];
         //delete player
         Game.remove(player);
     });
-   
+    */
+
     //When a key press message is being sent from the client, do the folloing:
-    socket.on('keyPress',function(data){
+    socket.on('keyPress', function (data) {
         //if the left key was pressed, move player left
-        if(data.inputId === 'left')
+        if (data.inputId === 'left')
             player.moveLeft();
         //if the right key was pressed move right
-        else if(data.inputId === 'right')
+        else if (data.inputId === 'right')
             player.moveRight();
         //if the up key was pressed move up
-        else if(data.inputId === 'up')
+        else if (data.inputId === 'up')
             player.moveUp();
         //if the down key was pressed move down
-        else if(data.inputId === 'down')
+        else if (data.inputId === 'down')
             player.moveDown();
     });
-   
+
 });
- 
-setInterval(function(){
-    var pack = [];
 
-    //Update players positions
-    for(var player in game.players){
-        var player = game.players[player];
 
-        //update players position
-        player.updatePosition();
+setInterval(function () {
 
-        var playersPosition = {
-            x: player.x,
-            y: player.y
-        };
+    /*
+        var pack = {};
 
-        pack.push(playersPosition);    
-    }
+        //Update players positions
+        for(var player in game.players){
+            var player = game.players[player];
 
-    //Update bombs positions
-    for (var bomb in game.bombs){
-        var bomb = game.bombs[bomb];
-        
-        var bombsPosition = {
-            x: bomb.x,
-            y: bomb.y
-        };
-        
-        pack.push(bombsPosition);
-    }
+            //update players position
+            player.updatePosition();
 
+            pack["playerPosition"] = {
+                x: player.x,
+                y: player.y
+            };    
+        }
+
+        //Update bombs positions
+        for (var bomb in game.bombs){
+            var bomb = game.bombs[bomb];
+            
+            
+            pack["bombsPosition"] = {
+                x: bomb.x,
+                y: bomb.y
+            };
+        }
+
+    */
+
+    var pack = {};
+
+    pack["players"] = [{
+            x: 500,
+            y: 500
+        },
+        {
+            x: 384,
+            y: 489
+        }
+    ];
+
+    pack["bombs"] = [{
+        x: 300,
+        y: 300
+    }, {
+        x: 700,
+        y: 700
+    }];
 
     //Update all clients states
-    for(var i in SOCKET_LIST){
+    for (var i in SOCKET_LIST) {
         var socket = SOCKET_LIST[i];
-        socket.emit('newPositions',pack);
+        socket.emit('newPositions', pack);
     }
-   
-},1000/25);
 
-
-
+}, 1000 / 25);
