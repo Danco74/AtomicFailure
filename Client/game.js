94f7bd05659f9404c11df8287a901c27e5b20e2e
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-09-28 14:19:31 +0300

    fix local score

--- Client/game.js
+++ Client/game.js
@@ -109,7 +109,7 @@ var GameState = {
                 y_count+=15;
             }
 
-            var scoreText = game.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#000' });
+            var scoreText = game.add.text(16, 16, 'Score: ' + data.score, { fontSize: '32px', fill: '#000' });
             for (var i = 0; i < data.bombs.length; i++) {
                 var bombPos = data.bombs[i];
                 var bomb = game.add.sprite(bombPos._x, bombPos._y, 'potions');
