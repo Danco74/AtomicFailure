0d3e0f51d66daa48c04924aa318f23638f183431
Lana Maidenbaum <lanamaidenbaum@Lanas-MacBook-Pro.local>
2017-10-01 14:35:01 +0300

    updated ALL capital conventions

--- Client/preload.js
+++ Client/preload.js
@@ -4,20 +4,20 @@ var PreloadState = {
 
         // this.load.image('logo', 'assets/RedFront.png');
         // this.logo =this.add.sprite(this.game.world.centerX,this.game.world.centerY,'logo').anchor.set(0.5);
-        this.load.image('background', 'assets/background.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.image('blast', 'assets/blast.png');
-        this.load.image('explosion', 'assets/explosion.png');
-        this.load.image('sink', 'assets/sink.png');
-        this.load.spritesheet('playerDown','assets/down.png',48,80,8);
-        this.load.spritesheet('playerUp','assets/up.png',47,68,8);
-        this.load.spritesheet('playerLeft','assets/left.png',40,55,4);
-        this.load.spritesheet('playerRight','assets/right.png',40,55,4);
-        this.load.spritesheet('potions','assets/potions.png',29,37,4);
-        this.load.spritesheet('logo','assets/potionshome.png',87,111,4);
-        this.load.spritesheet('explosion1','assets/explosion1.png',48,45,14);
-        this.load.audio('music', 'assets/music.mp3');
-        this.load.audio('explosionEffect', 'assets/explosion.mp3');
+        this.load.image('background', 'Assets/background.png');
+        this.load.image('bomb', 'Assets/bomb.png');
+        this.load.image('blast', 'Assets/blast.png');
+        this.load.image('explosion', 'Assets/explosion.png');
+        this.load.image('sink', 'Assets/sink.png');
+        this.load.spritesheet('playerDown','Assets/down.png',48,80,8);
+        this.load.spritesheet('playerUp','Assets/up.png',47,68,8);
+        this.load.spritesheet('playerLeft','Assets/left.png',40,55,4);
+        this.load.spritesheet('playerRight','Assets/right.png',40,55,4);
+        this.load.spritesheet('potions','Assets/potions.png',29,37,4);
+        this.load.spritesheet('logo','Assets/potionshome.png',87,111,4);
+        this.load.spritesheet('explosion1','Assets/explosion1.png',48,45,14);
+        this.load.audio('music', 'Assets/music.mp3');
+        this.load.audio('explosionEffect', 'Assets/explosion.mp3');
 
     },
 
